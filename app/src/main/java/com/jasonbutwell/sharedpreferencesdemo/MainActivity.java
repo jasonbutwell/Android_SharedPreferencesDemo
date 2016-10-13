57b0ee587feab939e5ae7191b4d71fd76731a702
Jason Butwell <jasonbutwell@gmail.com>
2016-10-13 01:23:56 +0100

    Modified the Action bar to have a menu icon. When this menu icon is clicked a result is sent to the Log.

--- app/src/main/java/com/jasonbutwell/sharedpreferencesdemo/MainActivity.java
+++ app/src/main/java/com/jasonbutwell/sharedpreferencesdemo/MainActivity.java
@@ -5,6 +5,8 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -32,6 +34,29 @@ public class MainActivity extends AppCompatActivity {
 
         TextView tv = (TextView) findViewById(R.id.TextView);
         tv.setText("Hello " + userName );
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.menu_add) {
+            Log.i("tapped","Button tapped");
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
