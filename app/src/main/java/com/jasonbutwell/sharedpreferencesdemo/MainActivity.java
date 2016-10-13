8b9d34e9f57f9144974301f804044cbe17ee0099
Jason Butwell <jasonbutwell@gmail.com>
2016-10-13 01:52:00 +0100

    The implementation of an Alert Dialog and onClick listeners.

--- app/src/main/java/com/jasonbutwell/sharedpreferencesdemo/MainActivity.java
+++ app/src/main/java/com/jasonbutwell/sharedpreferencesdemo/MainActivity.java
@@ -1,8 +1,10 @@
 package com.jasonbutwell.sharedpreferencesdemo;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -35,6 +37,27 @@ public class MainActivity extends AppCompatActivity {
 
         TextView tv = (TextView) findViewById(R.id.TextView);
         tv.setText("Hello " + userName );
+
+        // Shows an alert with Yes and No options and implements the listeners for those buttons
+        // button presses are sent to the logCat for now.
+
+        new AlertDialog.Builder(this)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setTitle("Are you sure?")
+                .setMessage("Do you really want to do this?")
+                .setPositiveButton("yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Log.i("Button tapped", "Yes");
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Log.i("Button tapped","No");
+                    }
+                })
+                .show();
     }
 
     @Override
