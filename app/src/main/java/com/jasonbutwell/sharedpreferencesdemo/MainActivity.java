f6562ba3407f921b46301baa16548065661d5a1f
Jason Butwell <jasonbutwell@gmail.com>
2016-10-13 01:43:29 +0100

    Show icons on actionbar permanently by setting the showAsAction to always.

--- app/src/main/java/com/jasonbutwell/sharedpreferencesdemo/MainActivity.java
+++ app/src/main/java/com/jasonbutwell/sharedpreferencesdemo/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -38,22 +39,42 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+
         // Inflate the menu; this adds items to the action bar if it is present.
+
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
+
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.menu_add) {
-            Log.i("tapped","Button tapped");
 
+        // Check to see if the first Options menu item was selected.
+
+        if (id == R.id.action_settings1) {
+            Log.i("select","Item 1 selected");
+            return true;
+        }
+
+        // CHeck to see if the second Options menu item was selected.
+
+        if (id == R.id.action_settings2) {
+            Log.i("select","Item 2 selected");
+            return true;
+        }
+
+        // CHeck to see if the third Options menu item was selected.
+
+        if (id == R.id.action_settings3) {
+            Log.i("select","Item 3 selected");
             return true;
         }
 
